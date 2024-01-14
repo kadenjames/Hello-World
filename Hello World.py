38112220a59e393ab31bfbf842ba30475f943160
kadenjames <kaden-james@hotmail.com>
2024-01-14 20:58:50 +0000

    Create Hello World.py
    My first computer code using python - utilising pseudocode, variables, input and print.

---
+++ Hello World.py
@@ -0,0 +1,12 @@
+# Variable for inputting names
+name = input("Enter your name: ")
+
+# Print greeting and name
+print("Hello, " + name + "!")
+
+# Variable for inputting age
+age = input("Enter your age: ")
+
+# Print age and 'hello world'
+print("You are: " + age)
+print("Hello World!")
